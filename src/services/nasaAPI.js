36b4929097e4ffa0648dcc20dc98be27daf5fb26
Iasmim Oliveira <oliv.iasmim@gmail.com>
2021-08-05 13:03:10 -0300

    gambiarra: api

--- src/services/nasaAPI.js
+++ src/services/nasaAPI.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 
-const apiKey = process.env.REACT_APP_NASA_KEY;
+// const apiKey = process.env.REACT_APP_NASA_KEY;
+
+// export const nasaAPI = axios.create({
+//     baseURL: `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`,
+// });
 
 export const nasaAPI = axios.create({
-    baseURL: `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`,
+    baseURL: `https://api.nasa.gov/planetary/apod?api_key=4FyIgMGadOtPxXMbdx8Chz3K4KpPEjWfJoHaFAeF`,
 });
